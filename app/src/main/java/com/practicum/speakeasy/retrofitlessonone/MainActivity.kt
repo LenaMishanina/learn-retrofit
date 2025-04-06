946319df704ba651a16bbbefbe6a2373b99fac10
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-04-06 20:39:16 +0300

    impl logging of request data

--- app/src/main/java/com/practicum/speakeasy/retrofitlessonone/MainActivity.kt
+++ app/src/main/java/com/practicum/speakeasy/retrofitlessonone/MainActivity.kt
@@ -10,6 +10,8 @@ import com.practicum.speakeasy.retrofitlessonone.retrofit.PostApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -22,9 +24,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
         val postApi = retrofit.create(PostApi::class.java)
 
